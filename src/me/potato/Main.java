9ffea871a8234d63e206b39c9b1b53a63f2b0e34
dongju.paek <dongju.paek@navercorp.com>
2022-03-03 01:08:49 +0900

    improve logic

--- src/me/potato/Main.java
+++ src/me/potato/Main.java
@@ -9,35 +9,24 @@ import java.util.stream.IntStream;
 public class Main {
 
   public static void main(String[] args) {
-    int totalDuration = 60 * 60 * 60; // 60 시간 짜리 쌉무식 동영상
+    int totalDuration = 10;
     // duration seconds
     List<View> views = new ArrayList<>();
-    views.add(new View(0, 100));
-    System.out.println(getProgressRatio(totalDuration, views));
+    views.add(new View(0, 4));
+    views.add(new View(8, 9));
 
-    views.add(new View(0, 100));
-    views.add(new View(0, 100));
-    System.out.println(getProgressRatio(totalDuration, views));
 
-    IntStream.range(100, 200).forEach((aNum) -> views.add(new View(aNum, 200)));
-    System.out.println(getProgressRatio(totalDuration, views));
-
-    IntStream.range(0, 100).forEach((aNum) -> views.add(new View(0, 200)));
-    System.out.println(getProgressRatio(totalDuration, views));
-
-    IntStream.range(100, 200).forEach((aNum) -> views.add(new View(aNum, 200)));
-    System.out.println(getProgressRatio(totalDuration, views));
-
-    IntStream.range(0, totalDuration / 2).forEach((aNum) -> views.add(new View(0, aNum)));
-    System.out.println(getProgressRatio(totalDuration, views));
-
+    System.out.println(getProgressRatio(totalDuration, views) / totalDuration * 100);
   }
 
+
   private static double getProgressRatio(double totalDuration, List<View> views) {
-    // 남자는 올림이다.
-    return Math.ceil(views.stream()
-            .map(view -> BigInteger.TWO.pow(view.end - view.start).subtract(BigInteger.ONE).shiftLeft(view.start))
-            .reduce(BigInteger.ZERO, (view1, view2) -> view1.or(view2)).bitCount() / totalDuration * 100);
+    double result = views.stream()
+            .map(view -> BigInteger.TWO.pow(view.end - view.start + 1).subtract(BigInteger.ONE)
+                    .shiftLeft(view.start))
+            .reduce(BigInteger.ZERO, (view1, view2) -> view1.or(view2)).bitCount()  ;
+
+    return result  ;
   }
 
   static class View {
@@ -52,6 +41,14 @@ public class Main {
         this.start = start;
         this.end   = end;
       }
+    }
+
+    @Override
+    public String toString() {
+      return "View{" +
+              "start=" + start +
+              ", end=" + end +
+              '}';
     }
   }
 }
